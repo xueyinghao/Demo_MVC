d3b7956849a1690c4e4712922652d0a329251bfd
xueyinghao <1095238121@qq.com>
2016-07-18 11:18:27 +0800

    xyh

---
+++ Demo_MVC/MyConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace Demo_MVC
+{
+    public class MyConfig:Entities
+    {
+        //封装EF实体模型
+        public System.Data.Entity.DbContext db { get; private set; }
+    }
+}
